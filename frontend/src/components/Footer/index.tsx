5cbe0f0057efd79c42321ee8e0343c14a2d0251f
salmoniscute <noralin249@gmail.com>
2024-12-01 14:26:32 +0800

    navibar , footer change color

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -9,12 +9,57 @@ import {
     useEffect,
 } from "react";
 import { Link } from "react-router-dom";
+import userDataContext from "../../context/userData";
 
 import "./index.scss";
 
 export default function Footer(): ReactElement {
+    const userData = useContext(userDataContext);
+    const [mbtiColorButton, setMbtiColorButton] = useState<string>("");
+    const [mbtiColorNotice, setMbtiColorNotice] = useState<string>("");
+    useEffect(() => {
+        setMbtiColor(userData ? userData.mbti : "ENFJ");
+    }, []);
+
+    const setMbtiColor = (mbti: string) => {
+        const categoryColorsBotton: { [key: string]: string } = {
+            Analysts: "blue",
+            Diplomats: "green",
+            Sentinels: "purple",
+            Explorers: "yellow",
+        };
+        const categoryColorsNotice: { [key: string]: string } = {
+            Analysts: "#C9EAFB",
+            Diplomats: "#B4E9B4",
+            Sentinels: "#E8CEED",
+            Explorers: "#FFFFBF",
+        };
+
+        const mbtiGroups: { [key: string]: string } = {
+            INTJ: "Analysts",
+            INTP: "Analysts",
+            ENTJ: "Analysts",
+            ENTP: "Analysts",
+            INFJ: "Diplomats",
+            INFP: "Diplomats",
+            ENFJ: "Diplomats",
+            ENFP: "Diplomats",
+            ISTJ: "Sentinels",
+            ISFJ: "Sentinels",
+            ESTJ: "Sentinels",
+            ESFJ: "Sentinels",
+            ISTP: "Explorers",
+            ISFP: "Explorers",
+            ESTP: "Explorers",
+            ESFP: "Explorers",
+        };
+
+        const group = mbtiGroups[mbti];
+        setMbtiColorButton(categoryColorsBotton[group] || "gray");
+        setMbtiColorNotice(categoryColorsNotice[group] || "gray");
+    };
     return (
-        <div id="footer">
+        <div id="footer" style={{ backgroundColor: mbtiColorNotice }}>
             <div></div>
         </div>
     );
--- frontend/src/components/NavigateBar/index.tsx
+++ frontend/src/components/NavigateBar/index.tsx
@@ -15,8 +15,51 @@ import "./index.scss";
 
 export default function NavigateBar(): ReactElement {
     const userData = useContext(userDataContext);
+    const [mbtiColorButton, setMbtiColorButton] = useState<string>("");
+    const [mbtiColorNotice, setMbtiColorNotice] = useState<string>("");
+    useEffect(() => {
+        setMbtiColor(userData ? userData.mbti : "ENFJ");
+    }, []);
+
+    const setMbtiColor = (mbti: string) => {
+        const categoryColorsBotton: { [key: string]: string } = {
+            Analysts: "blue",
+            Diplomats: "green",
+            Sentinels: "purple",
+            Explorers: "yellow",
+        };
+        const categoryColorsNotice: { [key: string]: string } = {
+            Analysts: "#C9EAFB",
+            Diplomats: "#B4E9B4",
+            Sentinels: "#E8CEED",
+            Explorers: "#FFFFBF",
+        };
+
+        const mbtiGroups: { [key: string]: string } = {
+            INTJ: "Analysts",
+            INTP: "Analysts",
+            ENTJ: "Analysts",
+            ENTP: "Analysts",
+            INFJ: "Diplomats",
+            INFP: "Diplomats",
+            ENFJ: "Diplomats",
+            ENFP: "Diplomats",
+            ISTJ: "Sentinels",
+            ISFJ: "Sentinels",
+            ESTJ: "Sentinels",
+            ESFJ: "Sentinels",
+            ISTP: "Explorers",
+            ISFP: "Explorers",
+            ESTP: "Explorers",
+            ESFP: "Explorers",
+        };
+
+        const group = mbtiGroups[mbti];
+        setMbtiColorButton(categoryColorsBotton[group] || "gray");
+        setMbtiColorNotice(categoryColorsNotice[group] || "gray");
+    };
     return (
-        <div id="navigateBar">
+        <div id="navigateBar" style={{ backgroundColor: mbtiColorNotice }}>
             <Link to="/" className="logo">
                 <h1>HIHI</h1>
             </Link>
